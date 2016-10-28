4a3087b64eda2c8cb9e5ae0e2df9169afff26288
mahendra <mayadav@visa.com>
2016-10-28 11:44:19 +0530

    fixed review comment for adding notes for delay

--- samples/Refund.java
+++ samples/Refund.java
@@ -16,11 +16,11 @@ public class Refund {
 	public static void main(String[] args) {
 		// Set ApiKey, secretKey and timeoutMilliseconds
 		ConfigurationBuilder builder = new ConfigurationBuilder();
-		builder.setApiKey("apikey")
-			.setSecretKey("secretkey")
-			.setTimeoutMilliseconds(30000);
-		String nextYear = Integer.toString((Calendar.getInstance().get(Calendar.YEAR) + 1));
-		
+		builder.setApiKey("apikey").setSecretKey("secretkey")
+				.setTimeoutMilliseconds(30000);
+		String nextYear = Integer.toString((Calendar.getInstance().get(
+				Calendar.YEAR) + 1));
+
 		Configuration config = builder.build();
 		SalesApi saleApi = new SalesApi(config);
 		RefundsApi refundApi = new RefundsApi(config);
@@ -35,23 +35,31 @@ public class Refund {
 		request.setAmount(new BigDecimal(5.00));
 		request.setCurrency("USD");
 		request.setReferenceId("123");
-		
+
 		try {
 			// Perform a sale
 			com.visa.payments.model.Sale sale = saleApi.createSale(request);
 			String saleId = sale.getId();
-			System.out.println("Sale created and returned with saleId: " + saleId);
-			
+			System.out.println("Sale created and returned with saleId: "
+					+ saleId);
+
+			// Please note that there could be a delay for the transaction
+			// detail to be available for retrieval after a requests is posted
+			// to the server for processing.
 			TimeUnit.SECONDS.sleep(2);
-			
+
 			RefundRequest refundRequest = new RefundRequest();
 			refundRequest.setAmount(new BigDecimal(5.00));
 			refundRequest.setReferenceId("123");
-			com.visa.payments.model.Refund refund = refundApi.refundSale(saleId, refundRequest);
+			com.visa.payments.model.Refund refund = refundApi.refundSale(
+					saleId, refundRequest);
 			System.out.println("Sale refunded: " + refund);
-			
+
+			// Please note that there could be a delay for the transaction
+			// detail to be available for retrieval after a requests is posted
+			// to the server for processing.
 			TimeUnit.SECONDS.sleep(2);
-			
+
 			// Retrieve a refund
 			GetRefund getRefund = refundApi.getRefund(refund.getId());
 			System.out.println("Retrieve refund: " + getRefund);
--- samples/SearchPaymentById.java
+++ samples/SearchPaymentById.java
@@ -1,12 +1,10 @@
 package com.visa.payments.samples;
 
-
 import com.visa.payments.ApiException;
 import com.visa.payments.Configuration;
 import com.visa.payments.Configuration.ConfigurationBuilder;
 import com.visa.payments.model.Transaction;
 import com.visa.payments.api.PaymentsApi;
-
 
 public class SearchPaymentById {
 	public static void main(String[] args) {
@@ -17,18 +15,20 @@ public class SearchPaymentById {
 
 		Configuration config = builder.build();
 		PaymentsApi paymentApi = new PaymentsApi(config);
-		if(args.length!=1){
-			System.out.println("Transaction ID is not provided in command line argument. ");
+		if (args.length != 1) {
+			System.out
+					.println("Transaction ID is not provided in command line argument. ");
+			System.exit(1);
+		} else if (args[0].equals("-help")) {
+			System.out
+					.println("Specify a transaction ID to return a specific payment resource"
+							+ "\n"
+							+ "Use below command to Search Payment by ID"
+							+ "\n " + "java SearchPaymentById [transaction ID]");
 			System.exit(1);
 		}
-		else if(args[0].equals("-help")){
-			System.out.println("Specify a transaction ID to return a specific payment resource" + "\n" + 
-					"Use below command to Search Payment by ID" + "\n "+ 
-					"java SearchPaymentById [transaction ID]");
-			System.exit(1);
-		}
-		//Specify a transaction ID to return a specific payment resource.
-		//This will get submitted authorizations, sales, captures, or refunds.
+		// Specify a transaction ID to return a specific payment resource.
+		// This will get submitted authorizations, sales, captures, or refunds.
 		String transactionId = args[0];
 		try {
 			Transaction transaction = paymentApi.getPayment(transactionId);
